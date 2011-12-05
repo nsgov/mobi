e4850ac616705b8fb969a6e5dff3f55a34857aa4
Dave Nordlund <nordludp@gov.ns.ca>
2011-12-05 09:37:38 -0400

    Add color to draw more attention to errors

--- bin/lastmod.py
+++ bin/lastmod.py
@@ -7,5 +7,9 @@ import sys, os, datetime
 
 if len(sys.argv) != 2:
 	sys.exit("Usage: " + sys.argv[0] + " filename")
-stat = os.stat(sys.argv[1])
+try:
+	stat = os.stat(sys.argv[1])
+except OSError as (errno, errmsg):
+	sys.stderr.write("[1;37;41m'{0}': {1}[K[0;39;49m\n".format(sys.argv[1], errmsg))
+	raise
 print datetime.date.fromtimestamp(stat[8])
